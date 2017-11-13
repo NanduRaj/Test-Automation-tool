d208dd2198e34e77f45f5fd95147ac197c357205
Nandu-auxo <32476582+Nandu-auxo@users.noreply.github.com>
2017-11-13 12:56:18 +0530

    Update MongoConnector.java

--- src/main/java/com/auxolabs/testAutomationTool/MongoConnector.java
+++ src/main/java/com/auxolabs/testAutomationTool/MongoConnector.java
@@ -21,17 +21,34 @@ public class MongoConnector {
         database = mongoClient.getDatabase(configuration.getMongoDatabase());
     }
 
+    /**
+     * function to insert a document to a collection in a database
+     * @param collectionName, collection in which document to be inserted
+     * @param toInsert, document to be inserted
+     */
     public void insert(String collectionName, Document toInsert){
         MongoCollection<Document> collection = database.getCollection(collectionName);
         collection.insertOne(toInsert);
     }
 
+    /**
+     * function to delete a document in a collection from a database
+     * @param collectionName, the collection where document to be deleted
+     * @param toDelete,,document to be deletes
+     * @return count of documents deleted
+     */
     public long delete(String collectionName, Document toDelete){
         MongoCollection<Document> collection = database.getCollection(collectionName);
         DeleteResult deleteResult = collection.deleteOne(toDelete);
         return deleteResult.getDeletedCount();
     }
 
+    /**
+     * function to fetch a document from a collection in a database
+     * @param collectionName,the collection from which the document is to be fetched
+     * @param query, the query document which matches the document to be found
+     * @return document fetched
+     */
     public Document getDocument(String collectionName,Document query){
         MongoCollection<Document> collection = database.getCollection(collectionName);
         FindIterable<Document> iterDoc = collection.find(query);
@@ -41,6 +58,12 @@ public class MongoConnector {
         return null;
     }
 
+    /**
+     * function to update a document
+     * @param collectionName, the collection in which the document is to be updated
+     * @param query, the query document which matches the document to be found
+     * @param updated, the updated document
+     */
     public void update(String collectionName, Document query, Document updated) {
         MongoCollection<Document> collection = database.getCollection(collectionName);
         UpdateResult updateResult = null;
@@ -54,6 +77,11 @@ public class MongoConnector {
         }
     }
 
+    /**
+     * function to get all documents in a collection
+     * @param collectionName, the collection from which documents to be found
+     * @return list of the documents present in the collection
+     */
     public List<Document> getAllDocuments(String collectionName){
         MongoCollection<Document> collection = database.getCollection(collectionName);
         MongoCursor<Document> cursor = collection.find().iterator();
